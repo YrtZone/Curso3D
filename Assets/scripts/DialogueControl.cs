c52f466dfa910f308d35b80401c003276aaa5d72
YrtZone <denilson15rodrigues@gmail.com>
2025-11-24 14:46:39 -0300

    Script de Dialouge Controller

--- Assets/scripts/DialogueControl.cs
+++ Assets/scripts/DialogueControl.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class DialogueControl : MonoBehaviour
 {
@@ -15,16 +16,75 @@ public class DialogueControl : MonoBehaviour
     private int index;
     private Coroutine typingCoroutine;
 
-
-
-    void Start()
-    {
-        
-    }
-
-  
     void Update()
     {
         
     }
+
+    public void Speech(string[] txt, string actorName)
+    {
+        if(typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        
+        dialogueObj.SetActive(true);
+        SpeetchText.text = "";
+        actorNameText.text = actorName;
+        sentences = txt;
+        index = 0;
+        typingCoroutine = StartCoroutine(TypeSentence());
+    }
+
+    IEnumerator TypeSentence()
+    {
+        SpeetchText.text = "";
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            SpeetchText.text += letter;
+            yield return new WaitForSeconds(TypingSpeed);
+        }
+        typingCoroutine = null;
+    }
+    public void NextSentence()
+    {
+        if(SpeetchText.text == sentences[index])
+        {
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                SpeetchText.text ="";
+                if(typingCoroutine != null)
+                {
+                    StopCoroutine(typingCoroutine);
+                }
+                typingCoroutine = StartCoroutine(TypeSentence());
+            }
+            else
+            {
+                EndDialogue();
+            }
+
+        }
+
+    }
+    
+    public void HidePanel()
+    {
+        if(typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        SpeetchText.text ="";
+        actorNameText.text ="";
+        index = 0;
+        dialogueObj.SetActive(false);
+    }
+    
+    public void EndDialogue()
+    {
+
+    }
+
+
 }
